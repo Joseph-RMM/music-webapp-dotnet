6ed0384762ba0ed6bf6a77e6a62654f6eacd6c1c
Joseph Maldonado <josephrogelio@hotmail.com>
2020-10-22 12:32:50 -0500

    Correccion del nav

--- Naver Music Web/Site.Master.designer.cs
+++ Naver Music Web/Site.Master.designer.cs
@@ -31,6 +31,24 @@ namespace Naver_Music_Web {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// userimg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image userimg;
+        
+        /// <summary>
+        /// username control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label username;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
