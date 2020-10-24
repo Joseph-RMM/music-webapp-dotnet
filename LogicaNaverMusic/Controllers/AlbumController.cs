bad18e0f10a406c58f80a2b641e8407f17239740
EmmanuelBazan <mqkz99@gmail.com>
2020-10-24 18:03:16 -0500

    Funcion VoteAlbum

--- LogicaNaverMusic/Controllers/AlbumController.cs
+++ LogicaNaverMusic/Controllers/AlbumController.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using LogicaNaverMusic.BaseDatos;
+
 namespace LogicaNaverMusic.Controllers
 {
     class AlbumController
     {
+        NaverMusicDBEntities modelDb = new NaverMusicDBEntities();
+
+        public void VoteAlbum(int idUser, int idAlbum, DateTime date)
+        {
+            VotoAlbum votoAlbum = new VotoAlbum();
+
+            votoAlbum.idAlbum = idAlbum;
+            votoAlbum.idUser = idUser;
+            votoAlbum.fecha = date;
+
+            modelDb.VotoAlbum.Add(votoAlbum);
+            modelDb.SaveChanges();
+        }
     }
 }
