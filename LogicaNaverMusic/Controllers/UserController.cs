95bb6e1aab82f0f0cc676be109cfe37fed2f9b6c
EmmanuelBazan <mqkz99@gmail.com>
2020-10-24 15:13:57 -0500

    Funcion CreateUser en la Capa logica

--- LogicaNaverMusic/Controllers/UserController.cs
+++ LogicaNaverMusic/Controllers/UserController.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using LogicaNaverMusic.BaseDatos;
+
 namespace LogicaNaverMusic.Controllers
 {
     class UserController
     {
+        private NaverMusicDBEntities modelDb = new NaverMusicDBEntities();
+        public bool CreateUser(string username, string password, string nombre, string apellido, string sexo,
+            string foto, string status, string correo, string telefono)
+        {
+            Usuarios user = new Usuarios();
+
+            user.username = username;
+            user.passsword = password;
+            user.nombre = nombre;
+            user.apellido = apellido;
+            user.sexo = sexo;
+            user.foto = foto;
+            user.estatus = status;
+            user.correo = correo;
+            user.telefono = telefono;
+
+            modelDb.Usuarios.Add(user);
+            modelDb.SaveChanges();
+
+            return true;
+
+        }
     }
 }
---
+++ LogicaNaverMusic/Models/UsuariosModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNaverMusic.Models
+{
+    class UsuariosModels
+    {
+        public int idUsuario { get; set; }
+        public string username { get; set; }
+        public string passsword { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string sexo { get; set; }
+        public string foto { get; set; }
+        public string estatus { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+    }
+}
