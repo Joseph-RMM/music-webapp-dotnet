91179512149c647c4834869618237a112b5f3d51
Joseph Maldonado <josephrogelio@hotmail.com>
2020-10-27 21:12:55 -0600

    ContentPlaceHolder Para MusicPlayer

--- Naver Music Web/Site.Master.designer.cs
+++ Naver Music Web/Site.Master.designer.cs
@@ -56,5 +56,14 @@ namespace Naver_Music_Web {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// AudioPlayerCPH control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder AudioPlayerCPH;
     }
 }
