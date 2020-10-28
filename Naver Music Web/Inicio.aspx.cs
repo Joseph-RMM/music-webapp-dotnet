1ffd39c2f21bbc6a30baece7a13ebb07ebf93cd9
Joseph Maldonado <josephrogelio@hotmail.com>
2020-10-27 21:20:26 -0600

    Agregado dinamico de elementos de canciones

--- Naver Music Web/Inicio.aspx.cs
+++ Naver Music Web/Inicio.aspx.cs
@@ -7,8 +7,76 @@ using System.Web.UI.WebControls;
 
 namespace Naver_Music_Web {
     public partial class Inicio : System.Web.UI.Page {
+        //Simulacion de datos recuperados de la base de datos
+        //TODO: Borrar cuando se implemente la base de datos
+        static int VotesDB = 13455;
+        static int VotesDB2 = 558;
         protected void Page_Load(object sender, EventArgs e) {
+            //Simulacion de consulta a la API y llenado del panel
+            panelMusic.Controls.Add(createMusicItem("https://pbs.twimg.com/media/DrEPUf1WsAIxCby.jpg", "POP/STARS", "K/DA", VotesDB, true, 1));
+            panelMusic.Controls.Add(createMusicItem("https://cdns-images.dzcdn.net/images/cover/3eddd7a427f3b4debe681e88e0811298/350x350.jpg", "THE BADDEST", "K/DA", VotesDB2, false, 2));
+        }
 
+        public Panel createMusicItem(string URLCover, string SongName, string ArtistName, int Votos, bool isFav, int SongID) {
+
+            Panel wrapper = new Panel {
+                CssClass = "wrappermusicitem"
+            };
+            //Div para los elementos del cover music
+            Panel playMusic = new Panel { CssClass = "wrapper-playmusic" };
+            Image cover = new Image {
+                ImageUrl = URLCover,
+                CssClass = "imgcovermusic"
+            };
+            ImageButton play = new ImageButton {
+                ImageUrl = "assets/playcover.png",
+                CssClass = "imgplaymusic"
+            };
+            playMusic.Controls.Add(cover);
+            playMusic.Controls.Add(play);
+            Label nombre = new Label {
+                Text = SongName,
+                CssClass = "songname"
+            };
+            Label artist = new Label {
+                Text = ArtistName,
+                CssClass = "artistname"
+            };
+            //Crear el wrapper-ratefav
+            Panel ratefav = new Panel { CssClass = "wrapper-ratefav" };
+            Button btnRate = new Button {
+                CssClass = "btn rate",
+                Text = "♥ " + Votos
+            };
+            btnRate.Click += delegate (object sender, EventArgs e) { RateClick(sender, e, SongID); };
+            Button btnFav = new Button {
+                CssClass = "btn fav",
+                Text = isFav ? "★" : "✩"
+            };
+            //Añadirlos al mini div
+            ratefav.Controls.Add(btnRate);
+            ratefav.Controls.Add(btnFav);
+            //Añadir todo al wrapper (panel) principal para devolverlo
+            wrapper.Controls.Add(playMusic);
+            wrapper.Controls.Add(nombre);
+            wrapper.Controls.Add(artist);
+            wrapper.Controls.Add(ratefav);
+            return wrapper;
+        }
+
+        public void RateClick(object sender, EventArgs e, int SongID) {
+            Button btnRate = (Button)sender;
+            int Votes = 0;
+            //Buscar los votos de la cancion con el ID
+            //TODO: Implementar codigo real y quitar if de simulacion
+            if (SongID == 1) { //SELECT Votos FROM VotosCancion WHERE IDCancion = ##
+                VotesDB++;
+                Votes = VotesDB;
+            } else {
+                VotesDB2++;
+                Votes = VotesDB2;
+            }
+            btnRate.Text = "♥ " + (Votes);
         }
     }
 }
--- Naver Music Web/Inicio.aspx.designer.cs
+++ Naver Music Web/Inicio.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace Naver_Music_Web {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txbBuscar;
+        
+        /// <summary>
+        /// panelMusic control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel panelMusic;
     }
 }
