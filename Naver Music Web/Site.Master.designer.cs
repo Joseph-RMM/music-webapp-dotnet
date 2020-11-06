c26c4b616414181db342640cbf6ebd496fb919f8
Joseph Maldonado <josephrogelio@hotmail.com>
2020-11-05 19:16:40 -0600

    Corazones

--- Naver Music Web/Site.Master.designer.cs
+++ Naver Music Web/Site.Master.designer.cs
@@ -49,6 +49,15 @@ namespace Naver_Music_Web {
         protected global::System.Web.UI.WebControls.Label username;
         
         /// <summary>
+        /// lblVotosRestantes control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblVotosRestantes;
+        
+        /// <summary>
         /// AudioPlayerCPH control.
         /// </summary>
         /// <remarks>
