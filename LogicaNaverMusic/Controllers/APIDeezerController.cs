23d5b240d61f7ad814f8df43ec99b51e42f3cd42
EmmanuelBazan <mqkz99@gmail.com>
2020-11-17 23:08:14 -0600

    metodo GetTrack(idTrack);

--- LogicaNaverMusic/Controllers/APIDeezerController.cs
+++ LogicaNaverMusic/Controllers/APIDeezerController.cs
@@ -14,6 +14,7 @@ namespace LogicaNaverMusic.Controllers
     public class APIDeezerController
     {
         private string url = "https://api.deezer.com/search?q=";
+        private string urlTrack = "https://api.deezer.com/track/";
 
         public List<Data> GetDataFromSearchDeezer(string busqueda)
         {
@@ -35,6 +36,28 @@ namespace LogicaNaverMusic.Controllers
             reponseFromSearch = JsonConvert.DeserializeObject<DeezerObject>(resultAPI);
 
             return reponseFromSearch.data;
+        }
+
+        public Data GetTrack(int idTrack)
+        {
+            WebRequest request = WebRequest.Create(urlTrack + idTrack.ToString());
+            request.Method = "GET";
+
+            HttpWebResponse reponse = null;
+            reponse = (HttpWebResponse)request.GetResponse();
+
+            string resultAPI;
+            using (Stream stream = reponse.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                resultAPI = sr.ReadToEnd();
+                sr.Close();
+            }
+
+            Data reponseFromSearch;
+            reponseFromSearch = JsonConvert.DeserializeObject<Data>(resultAPI);
+
+            return reponseFromSearch;
         }
     }
 }
