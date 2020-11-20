95497300d45b556c1c4711e3d62fe7a0672cc8cd
Joseph Maldonado <josephrogelio@hotmail.com>
2020-11-19 21:02:14 -0600

    Themify beta

---
+++ Naver Music Web/themes.js
@@ -0,0 +1,22 @@
+﻿const themeMap = {
+  naver: "dark",
+  dark: "solar",
+  solar: "naver"
+};
+
+const theme = localStorage.getItem('theme')
+  || (tmp = Object.keys(themeMap)[0],
+      localStorage.setItem('theme', tmp),
+      tmp);
+const bodyClass = document.body.classList;
+bodyClass.add(theme);
+
+function toggleTheme() {
+  const current = localStorage.getItem('theme');
+  const next = themeMap[current];
+
+  bodyClass.replace(current, next);
+  localStorage.setItem('theme', next);
+}
+
+document.getElementById('themeButton').onclick = toggleTheme;
