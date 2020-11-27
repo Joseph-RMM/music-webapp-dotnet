d0ab46987c4bb681a6354c39fb413b99daed5711
Joseph Maldonado <josephrogelio@hotmail.com>
2020-11-27 09:45:41 -0600

    Display user info

--- Naver Music Web/Landing.Master.cs
+++ Naver Music Web/Landing.Master.cs
@@ -8,7 +8,9 @@ using System.Web.UI.WebControls;
 namespace Naver_Music_Web {
     public partial class Landing : System.Web.UI.MasterPage {
         protected void Page_Load(object sender, EventArgs e) {
-
+            if (Session["userData"] != null) {
+                Response.Redirect("Inicio.aspx");
+            }
         }
     }
 }
--- Naver Music Web/Site.Master.cs
+++ Naver Music Web/Site.Master.cs
@@ -5,10 +5,32 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using LogicaNaverMusic;
+using LogicaNaverMusic.Controllers; //CONTROLADORES DE LOGICA NEGOCIOS
+using LogicaNaverMusic.Models;      //MODELOS DE LOGICA NEGOCIOS     
+using LogicaNaverMusic.BaseDatos;   //MODELOS DE BASE DE DATOS  
+
 namespace Naver_Music_Web {
     public partial class Site : System.Web.UI.MasterPage {
         protected void Page_Load(object sender, EventArgs e) {
-
+            if (Session["userData"] != null) {
+                //Display user info
+                UsuariosModels currentUser = (UsuariosModels) Session["userData"];
+                username.Text = currentUser.username;
+                try {
+                    userimg.ImageUrl = currentUser.foto;
+                } catch (Exception exception) {
+                    userimg.ImageUrl = "/assets/nouser.png";
+                }
+                VotoController votoController = new VotoController();
+                proc_GetVotesByUser_Result proc = new proc_GetVotesByUser_Result();
+                proc = votoController.GetVotesByUser(currentUser.idUsuario);
+                int votosUsados = proc.total ?? 0;
+                int votosRestantes = 100 - votosUsados;
+                lblVotosRestantes.Text = "♥ " + votosRestantes;
+            } else {
+                Response.Redirect("Default.aspx");
+            }
         }
     }
 }
