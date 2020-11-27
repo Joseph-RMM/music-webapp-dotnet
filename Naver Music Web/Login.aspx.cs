3c906138d26f62660ed8dea2dec400b6f9c77090
Joseph Maldonado <josephrogelio@hotmail.com>
2020-11-27 09:28:14 -0600

    Login

--- Naver Music Web/Login.aspx.cs
+++ Naver Music Web/Login.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using LogicaNaverMusic;
+using LogicaNaverMusic.Controllers; //CONTROLADORES DE LOGICA NEGOCIOS
+using LogicaNaverMusic.Models;      //MODELOS DE LOGICA NEGOCIOS     
+using LogicaNaverMusic.BaseDatos;   //MODELOS DE BASE DE DATOS   
+
 namespace Naver_Music_Web {
     public partial class Login : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
@@ -24,7 +29,23 @@ namespace Naver_Music_Web {
                 txbContrasena.CssClass = "txb error";
                 isCorrect = false;
             }
-            if (isCorrect) Response.Redirect("Inicio.aspx");
+
+
+            if (isCorrect) {
+                UserController userController = new UserController();
+                UsuariosModels currentUser = userController.LoginUser(txbEmail.Text, txbContrasena.Text);
+                if (currentUser.idUsuario != 0) {
+                    Session["userData"] = currentUser;
+                    if (chbAuto.Checked) {
+                        Session.Timeout = 525600;
+                    } else {
+                        //Las variables de sessión tienen un timeout de 20 minutos por default, por lo que no se recordará al usuario
+                    }
+                    Response.Redirect("Inicio.aspx");
+                } else {
+                    lblInfo.Text = "Usuario no registrado";
+                }
+            }
         }
     }
 }
