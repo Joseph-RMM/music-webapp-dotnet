0630b1a8fda5d70d65449e557b406783f80dae09
EmmanuelBazan <mqkz99@gmail.com>
2020-11-29 17:25:06 -0600

    Obtener dia actual

--- ConsoleAppResetVotes/Program.cs
+++ ConsoleAppResetVotes/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleAppResetVotes
@@ -10,7 +11,24 @@ namespace ConsoleAppResetVotes
     {
         static void Main(string[] args)
         {
+            Thread t = new Thread(PrintX);
+            t.Start();
+            t.Join();
+            //Console.ReadLine();
+                
+        }
 
+        static void PrintX()
+        {
+            bool b = true;
+            string date;
+            char c = '/';
+
+            while (b)
+            {
+                Console.WriteLine(date = DateTime.Now.ToShortDateString());
+                Thread.Sleep(1000);
+            }
         }
     }
 }
